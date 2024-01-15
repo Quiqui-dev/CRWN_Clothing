4882162247797f7dc28d14537a0484572a5d0f29
Kieran Mitchell <kieran@Kierans-MacBook-Pro.local>
2024-01-15 10:23:28 +0000

    adding navbar and sign in components. Sign in requires linking to an API

--- src/App.js
+++ src/App.js
@@ -1,38 +1,30 @@
-import CategoriesList from './components/categories_list/categories_list';
+import { Route, Routes} from "react-router-dom";
+import Home from "./components/routes/home/home";
+import NavigationBar from "./components/routes/navigationBar/navigationBar";
+import SignIn from "./components/routes/signIn/signIn";
+
+
+const Shop = () => {
+
+  return (
+    <div>
+      Shop page
+    </div>
+  );
+}
 
 const App = () => {
 
-  const categories = [
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ]
-  
-
   return (
-    <CategoriesList categories={categories} />
+
+    <Routes>
+      <Route path="/" element={<NavigationBar />}>
+        <Route index element={<Home />}/>
+        <Route path="shop" element={<Shop />}/>
+        <Route path="sign-in" element={<SignIn />}/>
+      </Route>
+    </Routes>
+    
   );
 }
 
